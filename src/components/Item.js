d30ecfc1fd54030641d35627f93d14b8b5ad1b5b
ketija-kaimina <97047352+ketija-kaimina@users.noreply.github.com>
2022-01-03 19:49:52 +0200

    Add files via upload

---
+++ src/components/Item.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Item.css";
+
+const Item = ({
+  id,
+  item,
+  list,
+  setEdit,
+  setEditId,
+  setItem,
+  setList,
+  complete,
+}) => {
+  const remove = (id) => {
+    setList(list.filter((el) => el.id !== id));
+  };
+
+  const handleComplete = (id) => {
+    setList(
+      list.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            complete: !item.complete,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleItem = (id) => {
+    const editItem = list.find((el) => el.id === id);
+    setItem(editItem.item);
+    setEdit(true);
+    setEditId(id);
+  };
+
+  return (
+    <div className="item">
+      <input
+        type="text"
+        value={item}
+        style={{
+          border: "none",
+          outline: "none",
+          backgroundColor: "transparent",
+          color: "#a339a0",
+          fontSize: "20px",
+        }}
+        className={complete ? "complete" : ""}
+      />
+      <img
+        style={{ cursor: "pointer" }}
+        onClick={() => remove(id)}
+        src="https://img.icons8.com/bubbles/50/000000/trash.png"
+        alt="Izdzēst"
+      />
+    </div>
+  );
+};
+export default Item;
